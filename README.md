8a47790330c52fea87cfcc3ccc7e70cd29e3b35e
Julien-Allard <julalldev@gmail.com>
2022-01-28 16:18:26 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CONTACT FORM BACK-END
+
+This is the part managing the backend from my contact form project.
+It allows my contact form to send me an email through a request on server (local or not).
+
+I'm using Express, Express-formidable, Mailgun
+
+@Julien Allard
---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(formidable());
+
+const api_key = process.env.API_KEY;
+const domain = process.env.DOMAIN;
+const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
+app.post("/form", (req, res) => {
+  const data = {
+    from: `${req.fields.firstname} ${req.fields.lastname} <${req.fields.email}>`,
+    to: "allardj85@gmail.com",
+    subject: `${req.fields.subject}`,
+    text: req.fields.message,
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    if (error === undefined) {
+      res.status(200).json({ message: "Données bien reçues, mail envoyé." });
+    } else {
+      res.status(400).json(error);
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server has started !");
+});
